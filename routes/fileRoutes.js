3335dba9872923594ee2baec1600bb3f579c3685
MOISE-FOFOU <moise2kuete@gmail.com>
2024-07-13 07:48:48 +0100

    rien

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-
 router.get('/files/:name', (req, res) => {
     const filepath = path.join(__dirname, '../uploads', req.params.name);
     res.sendFile(filepath, (err) => {
